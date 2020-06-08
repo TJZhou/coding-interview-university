7a8e590497e6c10d89878e2a15f6ae2e9a7a47fc
Tianju Zhou <zhou.tianj@husky.neu.edu>
2020-06-07 20:58:43 -0400

    Update DSPriorityQueue and test cases

--- src/main/com/tianju/ds/DSPriorityQueue.java
+++ src/main/com/tianju/ds/DSPriorityQueue.java
@@ -1,5 +1,6 @@
 package com.tianju.ds;
 
+import java.util.Collection;
 import java.util.Comparator;
 
 /**
@@ -8,11 +9,11 @@ import java.util.Comparator;
  *
  * @param <T>
  */
-public class DSPriorityQueue<T> implements DSQueue<T> {
+public class DSPriorityQueue<T extends Comparable<? super T>> implements DSQueue<T> {
 
     private final Comparator<? super T> comparator;
 
-    private static final int DEFAULT_CAPACITY = 11;
+    private static final int DEFAULT_CAPACITY = 10;
 
     private int capacity;
 
@@ -21,11 +22,11 @@ public class DSPriorityQueue<T> implements DSQueue<T> {
     private Object[] elements;
 
     public DSPriorityQueue() {
-        this(DEFAULT_CAPACITY, null);
+        this(DEFAULT_CAPACITY, Comparator.naturalOrder());
     }
 
     public DSPriorityQueue(int capacity) {
-        this(capacity, null);
+        this(capacity, Comparator.naturalOrder());
     }
 
     public DSPriorityQueue(Comparator<? super T> comparator) {
@@ -35,9 +36,21 @@ public class DSPriorityQueue<T> implements DSQueue<T> {
     public DSPriorityQueue(int capacity, Comparator<? super T> comparator) {
         if(capacity < 0)
             throw new IllegalArgumentException("Capacity cannot less than 0");
-        this.capacity = capacity;
         this.comparator = comparator;
+        this.capacity = capacity;
         this.elements = new Object[capacity];
+    }
+
+    public DSPriorityQueue(Collection<T> cs) {
+        this(cs, Comparator.naturalOrder());
+    }
+
+    public DSPriorityQueue(Collection<T> cs, Comparator<? super T> comparator) {
+        this.comparator = comparator;
+        this.capacity = (int) (cs.size() * 1.5);
+        this.elements = cs.toArray();
+        this.size = cs.size();
+        heapify();
     }
 
     public int size() {
@@ -48,36 +61,74 @@ public class DSPriorityQueue<T> implements DSQueue<T> {
         return size == 0;
     }
 
+    @SuppressWarnings("unchecked")
     public T get(int idx) {
         if(idx < 0 || idx >= size) {
             throw new ArrayIndexOutOfBoundsException(idx);
         }
-        @SuppressWarnings("unchecked")
-        T ele = (T) elements[idx];
-        return ele;
+        return (T) elements[idx];
     }
 
     public void offer(T t) {
-        size++;
+        if(size == capacity)
+            resize();
+        elements[size++] = t;
+        if(size >= 2)
+            siftUp(size-1, t);
     }
 
+    public void offer(T[] ts) {
+        for(T t : ts)
+            offer(t);
+    }
+
+    public void offer(Collection<T> ts) {
+        for(T t : ts)
+            offer(t);
+    }
+
+    @SuppressWarnings("unchecked")
     public T poll() {
-        return null;
+        if(size == 0)
+            return null;
+        T t = (T) elements[0];
+        swap(0, size-1);
+        elements[--size] = null;
+        if(size != 0)
+            siftDown(0, (T) elements[0]);
+        return t;
     }
 
+    private void swap(int idx1, int idx2) {
+        Object temp = elements[idx1];
+        elements[idx1] = elements[idx2];
+        elements[idx2] = temp;
+    }
+
+    @SuppressWarnings("unchecked")
     public T peek() {
-        return null;
+        if(size == 0)
+            return null;
+        return (T) elements[0];
     }
 
     public boolean remove(T t) {
-        size--;
-        return true;
+        int i = 0;
+        for(; i < size; i++) {
+            if(t.equals(elements[i])) {
+                removeAt(i);
+                return true;
+            }
+        }
+        return false;
     }
 
+    @SuppressWarnings("unchecked")
     public T removeAt(int idx) {
+        if(idx == 0)
+            return poll();
         if(idx < 0 || idx >= size)
             throw new ArrayIndexOutOfBoundsException(idx);
-        @SuppressWarnings("unchecked")
         T t = (T) elements[idx];
         System.arraycopy(elements, idx + 1, elements, idx, size - idx);
         elements[size--] = null;
@@ -91,15 +142,36 @@ public class DSPriorityQueue<T> implements DSQueue<T> {
         elements = newElements;
     }
 
+    @SuppressWarnings("unchecked")
     private void heapify() {
-
+        for(int i = size / 2 - 1; i >= 0; i--)
+            siftDown(i, (T) elements[i]);
     }
 
+    @SuppressWarnings("unchecked")
     private void siftUp(int idx, T t) {
-
+        while(idx > 0) {
+            int parent = (idx - 1) / 2;
+            if(comparator.compare(t, (T) elements[parent]) >= 0)
+                break;
+            swap(idx, parent);
+            idx = parent;
+        }
     }
 
-    private void sifDown(int idx, T t) {
-
+    @SuppressWarnings("unchecked")
+    private void siftDown(int idx, T t) {
+        int half = size / 2;
+        while(idx < half) {
+            int left = 2 * idx + 1;
+            int right= 2 * idx + 2;
+            int child = left;
+            if(right < size && (comparator.compare((T) elements[right], (T) elements[left]) <= 0))
+                child = right;
+            if(comparator.compare(t, (T) elements[child]) <= 0)
+                break;
+            swap(idx, child);
+            idx = child;
+        }
     }
 }
--- src/test/com/tianju/ds/DSPriorityQueueTest.java
+++ src/test/com/tianju/ds/DSPriorityQueueTest.java
@@ -1,10 +1,98 @@
 package com.tianju.ds;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
 /**
  * Tianju Zhou
  * Jun 6, 2020
  */
 public class DSPriorityQueueTest {
 
+    /**
+     * Min Heap Test
+     */
+    @Test
+    public void dsPriorityQueueTest() {
+        DSPriorityQueue<Integer> dsPriorityQueue = new DSPriorityQueue<>();
+        dsPriorityQueue.offer(new Integer[] {12,3,5,11,8,9,0,2,22,4,6,6,6});
+        Assertions.assertEquals(dsPriorityQueue.size(), 13);
+        Assertions.assertEquals(dsPriorityQueue.peek(), 0);
 
+        Assertions.assertEquals(dsPriorityQueue.get(0), 0);
+        Assertions.assertEquals(dsPriorityQueue.get(1), 2);
+        Assertions.assertEquals(dsPriorityQueue.get(2), 3);
+        Assertions.assertEquals(dsPriorityQueue.get(3), 8);
+        Assertions.assertEquals(dsPriorityQueue.get(4), 4);
+        Assertions.assertEquals(dsPriorityQueue.get(5), 6);
+        Assertions.assertEquals(dsPriorityQueue.get(6), 5);
+        Assertions.assertEquals(dsPriorityQueue.get(7), 12);
+        Assertions.assertEquals(dsPriorityQueue.get(8), 22);
+        Assertions.assertEquals(dsPriorityQueue.get(9), 11);
+        Assertions.assertEquals(dsPriorityQueue.get(10), 6);
+        Assertions.assertEquals(dsPriorityQueue.get(11), 9);
+        Assertions.assertEquals(dsPriorityQueue.get(12), 6);
+
+
+        Assertions.assertEquals(dsPriorityQueue.poll(), 0);
+        Assertions.assertEquals(dsPriorityQueue.poll(), 2);
+        Assertions.assertEquals(dsPriorityQueue.size(), 11);
+        Assertions.assertEquals(dsPriorityQueue.peek(), 3);
+
+        Assertions.assertEquals(dsPriorityQueue.removeAt(dsPriorityQueue.size() - 1), 6);
+        Assertions.assertTrue(dsPriorityQueue.remove(6));
+        Assertions.assertEquals(dsPriorityQueue.size(), 9);
+
+        dsPriorityQueue.offer(100);
+        Assertions.assertEquals(dsPriorityQueue.size(), 10);
+        Assertions.assertEquals(dsPriorityQueue.get(dsPriorityQueue.size() - 1), 100);
+
+        dsPriorityQueue.offer(0);
+        Assertions.assertEquals(dsPriorityQueue.size(), 11);
+        Assertions.assertEquals(dsPriorityQueue.get(0), 0);
+    }
+
+    /**
+     * Max Heap Test
+     */
+    @Test
+    public void dsPriorityQueueReverseOrderTest() {
+        DSPriorityQueue<Integer> dsPriorityQueue = new DSPriorityQueue<>(Collections.reverseOrder());
+        dsPriorityQueue.offer(new Integer[] {12,3,5,11,8,9,0,2,22,4,6,6,6});
+        Assertions.assertEquals(dsPriorityQueue.size(), 13);
+        Assertions.assertEquals(dsPriorityQueue.peek(), 22);
+
+        Assertions.assertEquals(dsPriorityQueue.get(0), 22);
+        Assertions.assertEquals(dsPriorityQueue.get(1), 12);
+        Assertions.assertEquals(dsPriorityQueue.get(2), 9);
+        Assertions.assertEquals(dsPriorityQueue.get(3), 11);
+        Assertions.assertEquals(dsPriorityQueue.get(4), 8);
+        Assertions.assertEquals(dsPriorityQueue.get(5), 6);
+        Assertions.assertEquals(dsPriorityQueue.get(6), 0);
+        Assertions.assertEquals(dsPriorityQueue.get(7), 2);
+        Assertions.assertEquals(dsPriorityQueue.get(8), 3);
+        Assertions.assertEquals(dsPriorityQueue.get(9), 4);
+        Assertions.assertEquals(dsPriorityQueue.get(10), 6);
+        Assertions.assertEquals(dsPriorityQueue.get(11), 5);
+        Assertions.assertEquals(dsPriorityQueue.get(12), 6);
+
+        Assertions.assertEquals(dsPriorityQueue.poll(), 22);
+        Assertions.assertEquals(dsPriorityQueue.poll(), 12);
+        Assertions.assertEquals(dsPriorityQueue.size(), 11);
+        Assertions.assertEquals(dsPriorityQueue.peek(), 11);
+
+        Assertions.assertEquals(dsPriorityQueue.removeAt(dsPriorityQueue.size() - 1), 5);
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> dsPriorityQueue.removeAt(-1));
+        Assertions.assertEquals(dsPriorityQueue.size(), 10);
+
+        dsPriorityQueue.offer(100);
+        Assertions.assertEquals(dsPriorityQueue.size(), 11);
+        Assertions.assertEquals(dsPriorityQueue.get(0), 100);
+
+        dsPriorityQueue.offer(0);
+        Assertions.assertEquals(dsPriorityQueue.size(), 12);
+        Assertions.assertEquals(dsPriorityQueue.get(dsPriorityQueue.size() - 1), 0);
+    }
 }
